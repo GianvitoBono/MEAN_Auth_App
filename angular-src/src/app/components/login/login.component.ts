21779a7f459f7788fb4fa774afd140f18586ea80
GianvitoBono <gianvitobono@gmail.com>
2017-05-30 18:59:24 +0200

    Aggiunto Login, Profilo e Logout

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username: String;
+  password: String;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      if(data.succes) {
+        this.authService.storeUser(data.token, data.user);
+        this.flashMessage.show('Accesso consentito', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(["dashboard"]);
+      } else {
+        this.flashMessage.show('Nome Utente o Password errati', {cssClass: 'alert-danger', timeout: 3000});
+      }
+    });
   }
 
 }
